be89b938d487368a12f30974afd247d0274412ba
agopalkr <agopalkr@ucsd.edu>
2024-07-23 03:17:01 +0000

    fixed job distribution

--- run_aries.sh
+++ run_aries.sh
@@ -6,5 +6,5 @@ do
 
     start=$(( (1024 / 8) * job_index ))
     end=$(( (1024 / 8) * (job_index + 1) -1 ))
-    aries run -j 1 -g 1 ag-${JOB}-${job_index} lingzhan/openvla -- bash -c "git clone https://github.com/akshaygopalkr/Depth-Anything-V2.git && cd Depth-Anything-V2 && ${RUN}"
+    aries run -j 1 -g 1 ag-${JOB}-${job_index} lingzhan/openvla -- bash -c "git clone https://github.com/akshaygopalkr/Depth-Anything-V2.git && cd Depth-Anything-V2 && ${RUN} ${start} ${end} ${job_index}"
 done
