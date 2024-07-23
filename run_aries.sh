bba78188283eea227b7cda6e62ab180b8b77198d
agopalkr <agopalkr@ucsd.edu>
2024-07-23 03:50:41 +0000

    fixed job distribution

--- run_aries.sh
+++ run_aries.sh
@@ -7,5 +7,5 @@ do
 
     start=$(( (1024 / 8) * job_index ))
     end=$(( (1024 / 8) * (job_index + 1) -1 ))
-    aries run ag-${JOB}-${job_index} -n ${nodes[${job_index}]} lingzhan/openvla -- bash -c "nvidia-smi && git clone https://github.com/akshaygopalkr/Depth-Anything-V2.git && cd Depth-Anything-V2 && ${RUN} ${start} ${end} ${job_index}"
+    aries run ag-${JOB}-${job_index} -n aries-b04 lingzhan/openvla -- bash -c "nvidia-smi && git clone https://github.com/akshaygopalkr/Depth-Anything-V2.git && cd Depth-Anything-V2 && ${RUN} ${start} ${end} ${job_index}"
 done
