5ca570c091a8a716777c3b223f2a78242b0d02af
agopalkr <agopalkr@ucsd.edu>
2024-07-23 01:39:58 +0000

    fixed batch size

--- run_aries.sh
+++ run_aries.sh
@@ -1,3 +1,4 @@
 RUN="bash generate_depth_maps.sh"
 JOB="DepthAnythingV2"
-aries run -j 8 -g 1 -n aries-b04 ag-${JOB} lingzhan/openvla -- bash -c "nvidia-smi && git clone https://github.com/akshaygopalkr/Depth-Anything-V2.git && cd Depth-Anything-V2 && ${RUN}"
+aries run -j 8 -g 1 -n aries-b03,aries-b04,aries-b05,aries-c01,aries-c02,aries-c03,aries-c04,aries-c05 ag-${JOB} lingzhan/openvla -- bash -c "git clone https://github.com/akshaygopalk
+r/Depth-Anything-V2.git && cd Depth-Anything-V2 && ${RUN}"
