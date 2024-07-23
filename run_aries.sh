69e96de966743bdc4d5f96a1cab12ef5a7c0363e
agopalkr <agopalkr@ucsd.edu>
2024-07-23 04:43:38 +0000

    gpu problem

--- run_aries.sh
+++ run_aries.sh
@@ -12,4 +12,4 @@ nodes=("aries-b03", "aries-b04", "aries-b05", "aries-c01", "aries-c02", "aries-c
 
 start=$(( (1024 / 8) * 0 ))
 end=$(( (1024 / 8) * (0 + 1) -1 ))
-aries run ag-${JOB}-0 -j 1 lingzhan/openvla -- bash -c "nvidia-smi && git clone https://github.com/akshaygopalkr/Depth-Anything-V2.git && cd Depth-Anything-V2 && ${RUN} ${start} ${end} 0"
+aries run ag-${JOB}-0 -j 1 lingzhan/openvla -- bash -c "git clone https://github.com/akshaygopalkr/Depth-Anything-V2.git && cd Depth-Anything-V2 && ${RUN} ${start} ${end} 0"
