5f8802245567de5ccb53fc45718cb0a0cc3dc0d7
agopalkr <agopalkr@ucsd.edu>
2024-07-30 04:28:41 +0000

    fixed script

--- run_length_aries.sh
+++ run_length_aries.sh
@@ -1,3 +1,3 @@
 RUN="bash get_dataset_length.py --data-dir /ariesdv0/zhanling/oxe-data-converted"
 JOB="debug"
-aries run -g 0 ag-${JOB} lingzhan/openvla -- bash -c "nvidia-smi && git clone https://github.com/akshaygopalkr/Depth-Anything-V2.git && cd Depth-Anything-V2 && ${RUN}"
+aries run -g 0 ag-${JOB} lingzhan/openvla -- bash -c "git clone https://github.com/akshaygopalkr/Depth-Anything-V2.git && cd Depth-Anything-V2 && ${RUN}"
