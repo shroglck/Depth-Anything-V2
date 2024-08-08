d5fad1973d5fbf4f60eb40cc50c53d195c9e11c1
akshaygopalkr <akshaygopalkrishnan@Akshays-MacBook-Pro.local>
2024-08-08 12:56:33 -0700

    update

--- dataset-scripts/generate_key_objects_dataset.sh
+++ dataset-scripts/generate_key_objects_dataset.sh
@@ -9,7 +9,7 @@ echo "Job index: $job_index, start: $start, end: $end"
 # Inner loop from start to end
 for (( j=start; j<=end; j++ ))
 do
-    python dataset-scripts/get_key_objects.py --data-shard $j --data-dir /ariesdv0/zhanling/oxe-data-converted --checkpoint-path /ariesdv0/zhanling/checkpoints
+    python dataset-scripts/get_key_objects.py --data-shard $j --data-dir /ariesdv0/zhanling/oxe-data-converted
     python dataset-scripts/save_key_objects.py --data-shard $j --data-dir /ariesdv0/zhanling/oxe-data-converted --obj-data-dir /ariesdv0/zhanling/oxe-data-converted/fractal20220817_obj_data/0.1.0
 done
 
