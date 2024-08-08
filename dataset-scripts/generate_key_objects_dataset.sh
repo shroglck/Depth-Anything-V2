48a6056bbafbfd996adbb1b2e642238f1a396286
akshaygopalkr <akshaygopalkrishnan@Akshays-MacBook-Pro.local>
2024-08-08 15:40:04 -0700

    update

--- dataset-scripts/generate_key_objects_dataset.sh
+++ dataset-scripts/generate_key_objects_dataset.sh
@@ -13,6 +13,3 @@ do
     python dataset-scripts/get_key_objects.py --data-shard $j --data-dir /ariesdv0/zhanling/oxe-data-converted --openai-key ${api_key}
     python dataset-scripts/save_key_objects.py --data-shard $j --data-dir /ariesdv0/zhanling/oxe-data-converted --obj-data-dir /ariesdv0/zhanling/oxe-data-converted/fractal20220817_obj_data/0.1.0
 done
-
-# Save positional words into single file
-python dataset-scripts gather_positional_words.py --data-dir /ariesdv0/zhanling/oxe-data-converted
