79deb96506edeecde90e7bc838b422424edeb741
agopalkr <agopalkr@ucsd.edu>
2024-08-10 16:22:26 +0000

    removed openai api

--- dataset-scripts/get_key_objects.py
+++ dataset-scripts/get_key_objects.py
@@ -1,64 +1,106 @@
-# minimum working example to load a single OXE dataset
-from PIL import Image
 import tensorflow as tf
 import tensorflow_datasets as tfds
-import torch
-from openai import AzureOpenAI
+# from openai import AzureOpenAI
 import argparse
 import re
 import pickle
 import time
 import json
 import os
-POS_PATTERN = r'\((.*?)\)'
-API_KEY = os.getenv("OPENAPI_KEY")
-ENDPOINT = "https://zhan-westus-0.openai.azure.com"
-ORGANIZATION = "zhan-westus-0-global"
-IGNORE = {'close', 'near', 'far', 'white', 'blueberry'}
+import string
+import spacy
+nlp = spacy.load("en_core_web_sm")
+import nltk
+nltk.download('stopwords')
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from nltk import word_tokenize, sent_tokenize
+lemmatizer = WordNetLemmatizer()
+stop = set(stopwords.words('english') + list(string.punctuation))
+IGNORE = {'place', 'close', 'knock', 'move', 'open'}
+POSITIONAL_WORDS = {'left', 'right', 'top', 'bottom', 'front', 'back', 'middle'}
 
-task_desc = "You will be given a sentence which are instructions for a robot to perform a certain task. Answer with just a list of the key objects that are part of the task. If the key object has positional information (e.g top, middle, left, right), then include the positional information in parantheses. The positional information must be exact to be included, so descriptors like near, close, or far should not be included. If an object has multiple related descriptions, don't separate them by commas.\n\nExamples:\n{in_context_examples}"
-in_context_examples = '\n'.join(["Task: place the grasped shiny green rxbar chocolate in the bottom drawer\nAnswer: shiny green rxbar chocolate, drawer (bottom)", "Task: move the middle Sprite can on top of the blue plate\nAnswer: Sprite can (middle), blue plate", "Task: Grab the green chip bag from the left table and put it in the white bowl. \nAnswer: green chip bag, table (left), white bowl"])
-task_desc = task_desc.format(in_context_examples=in_context_examples)
+must_contain_alphe = True
+min_term_len = 3
+
+def noun_phrase_by_sentence(text):
+    tokenized_content = []
+    sentences = []
+    for s in sent_tokenize(text):
+        for ss in s.split('\n'):
+            for sss in ss.split('     '):
+                if len(sss.strip()) > 0:
+                    sentences.append(sss.strip())
 
 
-def call_chatgpt(chatgpt_messages, model="gpt-4-1106-preview"):
+    np_list = []
+    for sentence in sentences:
+        np_sentence_list = []
+        doc = nlp(sentence)
+        for noun_chunk in doc.noun_chunks:
+            sent_stop = [i for i in word_tokenize(noun_chunk.text.lower()) if i not in stop]
+            # print ('sent_stop', sent_stop)
+            cleaned_sent_stop = []
+            for ss in sent_stop:
+                if len(ss) >= min_term_len and any(c.isalpha() for c in ss):
+                    cleaned_sent_stop.append(ss)
+
+            # print ('cleaned_sent_stop', cleaned_sent_stop)
+            lemmatized = []
+            if len(cleaned_sent_stop) > 0:
+                lemmatized=[lemmatizer.lemmatize(word) for word in cleaned_sent_stop]
+
+            if len(lemmatized) > 0:
+                # print ('lemmatized', lemmatized)
+                for n in lemmatized:
+                    np_sentence_list.append(n)
+                np_sentence_list.append("_".join(lemmatized))
+
+        if len(np_sentence_list) > 0:
+            np_list.append(list(set(np_sentence_list)))
+            # print('np_sentence_list', np_sentence_list)
+    return np_list
+
+def get_object_list(prompt):
     
-    # Call ChatGPT to get the key objects
-    response = client.chat.completions.create(
-        model=model, messages=chatgpt_messages
-    )
-    reply = response.choices[0].message.content
-    object_list = reply.split(',')
-    object_list = [obj.strip() for obj in object_list]
-    final_obj_list = []
-    pos_list = []
     
-    # Find all the positional words in the object list
-    for object in object_list:
-        pos_info = re.findall(POS_PATTERN, object)
-        if len(pos_info) == 0 or pos_info[0] in IGNORE:
-            pos_list.append('')
-        else:
-            pos_list.append(pos_info[0])
+    object_list = []
+    position_list = []
+    
+    object_list = noun_phrase_by_sentence(prompt)[0]
+    final_object_list = []
+    
+    for i in range(len(object_list)):
         
-        final_obj_list.append(re.sub(REMOVE_POS_PATTERN, '', object))
-        
-    return final_obj_list, pos_list
-
+        word = object_list[i]
+        # Check if the word is a subset of another word in the object list
+        if not any([word in object_list[idx] and word != object_list[idx] for idx in range(len(object_list))]):
+            
+            if '_' in word:
+                word = word.replace('_', ' ')
+            for ignore_word in IGNORE:
+                if ignore_word in word:
+                    word = word.replace(ignore_word, '')
+            
+            if word:
+                for pos_word in POSITIONAL_WORDS:
+                    if pos_word in word:
+                        position_list.append(pos_word)
+                        word = word.replace(pos_word, '')
+                        break
+                else:
+                    position_list.append('')
+                
+                final_object_list.append(word.strip())
     
-def prepare_chatgpt_message(task_prompt):
-    system_message = task_desc
-    messages = [{"role": "system", "content": system_message}]
-    messages += [{"role": "user", "content": task_prompt}]
-    return messages
+    prompt_words = prompt.split(' ')
+    for i in range(len(prompt_words)):
+        if prompt_words[i] == 'can':
+            previous_word = prompt_words[i - 1]
+            if previous_word in final_object_list:
+                final_object_list[final_object_list.index(previous_word)] = f'{previous_word} can'
 
-
-def get_object_list(task_batch):
-    
-    batched_requests = prepare_chatgpt_message(task_batch)
-    full_object_list, position_list = call_chatgpt(batched_requests)
-    
-    return full_object_list, position_list
+    return final_object_list, position_list
 
 def add_index(example, index):
     
@@ -80,26 +122,19 @@ def params():
 if __name__ == '__main__':   
     
     params = params()
-    num_gpus = torch.cuda.device_count()
     shard = params.data_shard
     split = f'train[{shard}shard]'
     
     shard_str_length = 5 - len(str(shard))
     shard_str = '0' * shard_str_length + str(shard)
     
-    dataset = tfds.load('fractal20220817_data', data_dir=params.data_dir,
+    dataset = tfds.load('fractal20220817_depth_data', data_dir=params.data_dir,
                         split=split)
     
     data_dict = {'idx': [idx for idx in range(len(dataset))]}
     data_idx = tf.data.Dataset.from_tensor_slices(data_dict)
     dataset = tf.data.Dataset.zip((dataset, data_idx))
     dataset = dataset.map(add_index, num_parallel_calls=1)
-    
-    client = AzureOpenAI(
-        api_key=API_KEY,
-        azure_endpoint=ENDPOINT,
-        api_version="2023-05-15"
-    )
     
     shard = params.data_shard
     split = f'train[{shard}shard]'
@@ -132,16 +167,7 @@ if __name__ == '__main__':
         
         # Get key objects and position words
         object_list, pos_list = get_object_list(task)
-        print(object_list, pos_list)
-        
-        for pos in pos_list:
-            if pos:
-                pos_vocab.add(pos)
-                
-        object_dict[f'{example_idx}'] = {
-            'objects': object_list,
-            'positions': pos_list
-        }
+        print(task, object_list, pos_list)
     
     # Save pickle file with key objects and positional words
     with open(params.pickle_file_path, 'wb') as f:
