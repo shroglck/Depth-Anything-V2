1d389022b55a3c2c0687e3f3fbc594e985018ee1
akshaygopalkr <akshaygopalkrishnan@Akshays-MacBook-Pro.local>
2024-08-28 14:57:19 -0700

    openai error

--- dataset-scripts/tracking-scripts/save_pick_tasks.sh
+++ dataset-scripts/tracking-scripts/save_pick_tasks.sh
@@ -9,5 +9,5 @@ echo "Job index: $job_index, start: $start, end: $end"
 # Inner loop from start to end
 for (( j=start; j<=end; j++ ))
 do
-    python dataset-scripts/tracking-scripts/save_pick_tasks.py --data-shard $j --data-dir /ariesdv0/zhanling/oxe-data-converted --obj-data-dir /ariesdv0/zhanling/oxe-data-converted/fractal20220817_obj_data/0.1.0
+    python dataset-scripts/tracking-scripts/save_pick_tasks.py --data-shard $j --data-dir /ariesdv0/zhanling/oxe-data-converted --obj-data-dir /ariesdv0/zhanling/oxe-data-converted/fractal20220817_pick_data/0.1.0
 done
