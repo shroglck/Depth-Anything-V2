6059dfc57c2e16a2c6de18e690efff8c98dec0d0
agopalkr <agopalkr@ucsd.edu>
2024-09-02 04:23:57 +0000

    edit lengths for seg dataset

--- dataset-scripts/tracking-scripts/fix_pick_shards.py
+++ dataset-scripts/tracking-scripts/fix_pick_shards.py
@@ -1,4 +1,5 @@
 import os, json, argparse
+import tensorflow_datasets as tfds
 
 
 if __name__ == '__main__':
@@ -8,26 +9,42 @@ if __name__ == '__main__':
     args = args.parse_args()
     shard_lengths = []
     
-    for i in range(1025):
-        with open(os.path.join(args.data_dir, 'fractal20220817_pick_data/0.1.0', f'num_examples_{i}.json'), 'r') as f:
-            
-            # Load the shard length and append it to the list
-            num_examples = json.load(f)[0]
-            shard_lengths.append(str(num_examples))
-            os.remove(os.path.join(args.data_dir, 'fractal20220817_pick_data/0.1.0', f'num_examples_{i}.json'))
-    
     dset_info_path = os.path.join(
         args.data_dir,
-        'fractal20220817_obj_data',
+        'fractal20220817_pick_data',
         '0.1.0',
         'dataset_info.json'
     )
-    
     with open(dset_info_path, 'r') as f:
         dset_info = json.load(f)
     
-    # Update the shard lengths
-    dset_info["splits"][0]["shardLengths"] = shard_lengths
+    for shard in range(1024):
+        
+        split = f'train[{shard}shard]'
+        print(f'Getting # of examples in {split}')
     
-    with open(os.path.join(args.data_dir, 'fractal20220817_pick_data/0.1.0', 'dataset_info.json'), 'w') as f:
+        shard_str_length = 5 - len(str(shard))
+        shard_str = '0' * shard_str_length + str(shard)
+        
+        # Load pickle file and dataset/record dataset
+        dataset = tfds.load('fractal20220817_pick_data', data_dir=args.data_dir,
+                            split=split)
+
+        num_examples = 0
+        
+        # Get the number of examples in the shard
+        for example in dataset:
+            
+            task = [e['observation']['natural_language_instruction'] for e in example['steps'].take(1)][0]
+            
+            if task:
+                num_examples += 1
+        
+        
+        dset_info["splits"][0]["shardLengths"][shard] = str(num_examples)
+    
+    
+    dset_info['name'] = 'fractal20220817_seg_data'
+    with open(os.path.join(args.data_dir, 'fractal20220817_seg_data', '0.1.0', 'dataset_info.json'), 'w') as f:
         json.dump(dset_info, f, indent=6)
+        
