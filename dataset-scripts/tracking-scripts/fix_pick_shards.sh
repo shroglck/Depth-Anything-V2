7600291d744290be035ca83f83ffe7f242609105
agopalkr <agopalkr@ucsd.edu>
2024-09-02 05:19:45 +0000

    edit lengths for seg dataset

--- dataset-scripts/tracking-scripts/fix_pick_shards.sh
+++ dataset-scripts/tracking-scripts/fix_pick_shards.sh
@@ -2,5 +2,5 @@
 # Inner loop from start to end
 for (( j=0; j<=1023; j++ ))
 do
-    python dataset-scripts/tracking-scripts/save_pick_tasks.py --data-shard $j
+    python dataset-scripts/tracking-scripts/fix_pick_shards.py --data-shard $j
 done
