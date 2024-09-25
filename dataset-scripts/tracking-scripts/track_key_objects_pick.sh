d6beef3667fea8c3210027089f6dfeb42c6c9bfb
akshaygopalkr <akshaygopalkrishnan@Akshays-MacBook-Pro.local>
2024-09-25 14:47:28 -0600

    shard debug

--- dataset-scripts/tracking-scripts/track_key_objects_pick.sh
+++ dataset-scripts/tracking-scripts/track_key_objects_pick.sh
@@ -15,5 +15,5 @@ echo "Job index: $job_index, start: $start, end: $end"
 #done
 python dataset-scripts/tracking-scripts/track_key_objects_pick.py --data-shard 100 --data-dir /ariesdv0/zhanling/oxe-data-converted
 python dataset-scripts/tracking-scripts/save_tracking_data.py --data-shard 100 --data-dir /ariesdv0/zhanling/oxe-data-converted --tracking-data-dir /ariesdv0/zhanling/oxe-data-converted/fractal20220817_tracking_data/0.1.0
-python dataset-scripts/tracking-scripts/track_key_objects_pick.py --data-shard 103 --data-dir /ariesdv0/zhanling/oxe-data-converted
-python dataset-scripts/tracking-scripts/save_tracking_data.py --data-shard 103 --data-dir /ariesdv0/zhanling/oxe-data-converted --tracking-data-dir /ariesdv0/zhanling/oxe-data-converted/fractal20220817_tracking_data/0.1.0
+#python dataset-scripts/tracking-scripts/track_key_objects_pick.py --data-shard 103 --data-dir /ariesdv0/zhanling/oxe-data-converted
+#python dataset-scripts/tracking-scripts/save_tracking_data.py --data-shard 103 --data-dir /ariesdv0/zhanling/oxe-data-converted --tracking-data-dir /ariesdv0/zhanling/oxe-data-converted/fractal20220817_tracking_data/0.1.0
