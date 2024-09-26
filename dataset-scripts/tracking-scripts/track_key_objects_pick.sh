c7219d29625d840fe5185efd348a2c2a39e6166a
akshaygopalkr <akshaygopalkrishnan@Akshays-MacBook-Pro.local>
2024-09-26 13:39:54 -0700

    shard debug

--- dataset-scripts/tracking-scripts/track_key_objects_pick.sh
+++ dataset-scripts/tracking-scripts/track_key_objects_pick.sh
@@ -7,13 +7,13 @@ job_index=$3
 echo "Job index: $job_index, start: $start, end: $end"
 
 # Inner loop from start to end
-#for (( j=start; j<=end; j++ ))
-#do
-#    python dataset-scripts/tracking-scripts/track_key_objects_pick.py --data-shard $j --data-dir /ariesdv0/zhanling/oxe-data-converted
-#    python dataset-scripts/tracking-scripts/save_tracking_data.py --data-shard $j --data-dir /ariesdv0/zhanling/oxe-data-converted --tracking-data-dir /ariesdv0/zhanling/oxe-data-converted/fractal20220817_tracking_data/0.1.0
-#    # python dataset-scripts/tracking-scripts/track_key_objects_pick.py --data-shard $j
-#done
-python dataset-scripts/tracking-scripts/track_key_objects_pick.py --data-shard 100 --data-dir /ariesdv0/zhanling/oxe-data-converted
-python dataset-scripts/tracking-scripts/save_tracking_data.py --data-shard 100 --data-dir /ariesdv0/zhanling/oxe-data-converted --tracking-data-dir /ariesdv0/zhanling/oxe-data-converted/fractal20220817_tracking_data/0.1.0
+for (( j=start; j<=end; j++ ))
+do
+    python dataset-scripts/tracking-scripts/track_key_objects_pick.py --data-shard $j --data-dir /ariesdv0/zhanling/oxe-data-converted
+    python dataset-scripts/tracking-scripts/save_tracking_data.py --data-shard $j --data-dir /ariesdv0/zhanling/oxe-data-converted --tracking-data-dir /ariesdv0/zhanling/oxe-data-converted/fractal20220817_tracking_data/0.1.0
+    # python dataset-scripts/tracking-scripts/track_key_objects_pick.py --data-shard $j
+done
+#python dataset-scripts/tracking-scripts/track_key_objects_pick.py --data-shard 100 --data-dir /ariesdv0/zhanling/oxe-data-converted
+#python dataset-scripts/tracking-scripts/save_tracking_data.py --data-shard 100 --data-dir /ariesdv0/zhanling/oxe-data-converted --tracking-data-dir /ariesdv0/zhanling/oxe-data-converted/fractal20220817_tracking_data/0.1.0
 #python dataset-scripts/tracking-scripts/track_key_objects_pick.py --data-shard 103 --data-dir /ariesdv0/zhanling/oxe-data-converted
 #python dataset-scripts/tracking-scripts/save_tracking_data.py --data-shard 103 --data-dir /ariesdv0/zhanling/oxe-data-converted --tracking-data-dir /ariesdv0/zhanling/oxe-data-converted/fractal20220817_tracking_data/0.1.0
