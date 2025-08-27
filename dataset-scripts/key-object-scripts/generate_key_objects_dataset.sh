91a317cd44eb6cd479d3913c1f7a3ab251831d8a
Shresth Grover <86047369+shroglck@users.noreply.github.com>
2025-08-27 23:53:18 +0530

    Update generate_key_objects_dataset.sh

--- dataset-scripts/key-object-scripts/generate_key_objects_dataset.sh
+++ dataset-scripts/key-object-scripts/generate_key_objects_dataset.sh
@@ -9,6 +9,6 @@ echo "Job index: $job_index, start: $start, end: $end"
 # Inner loop from start to end
 for (( j=start; j<=end; j++ ))
 do
-    python dataset-scripts/get_key_objects.py --data-shard $j --data-dir /ariesdv0/zhanling/oxe-data-converted
-    python dataset-scripts/save_key_objects.py --data-shard $j --data-dir /ariesdv0/zhanling/oxe-data-converted --obj-data-dir /ariesdv0/zhanling/oxe-data-converted/fractal20220817_obj_data/0.1.0
+    python dataset-scripts/get_key_objects.py --data-shard $j --data-dir /ariesdv0/openvla/OXE/rlds_mod
+    python dataset-scripts/save_key_objects.py --data-shard $j --data-dir /ariesdv0/openvla/OXE/rlds_mod --obj-data-dir /ariesdv0/openvla/OXE/rlds_mod/fractal20220817_obj_data/0.1.0
 done
