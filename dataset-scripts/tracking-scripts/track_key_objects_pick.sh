e246bbeda9c996bfe7d3deaf6cb64199153554b3
Shresth Grover <86047369+shroglck@users.noreply.github.com>
2025-08-28 22:32:51 +0530

    Update track_key_objects_pick.sh

--- dataset-scripts/tracking-scripts/track_key_objects_pick.sh
+++ dataset-scripts/tracking-scripts/track_key_objects_pick.sh
@@ -9,7 +9,7 @@ echo "Job index: $job_index, start: $start, end: $end"
 # Inner loop from start to end
 for (( j=start; j<=end; j++ ))
 do
-    python dataset-scripts/tracking-scripts/track_key_objects_pick.py --data-shard $j --data-dir /ariesdv0/openvla/OXE/rlds_mod
+    python dataset-scripts/tracking-scripts/track_key_objects.py --data-shard $j --data-dir /ariesdv0/openvla/OXE/rlds_mod
     python dataset-scripts/tracking-scripts/save_tracking_data.py --data-shard $j --data-dir /ariesdv0/openvla/OXE/rlds_mod --tracking-data-dir /ariesdv0/openvla/OXE/rlds_mod/fractal20220817_tracking_data/0.1.0
     # python dataset-scripts/tracking-scripts/track_key_objects_pick.py --data-shard $j
 done
